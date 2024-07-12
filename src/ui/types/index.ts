2fe4525de9eac3e9c7d9de8127adaaf023860f49
leejeongjun <backbone94@ajou.ac.kr>
2024-07-12 17:37:03 +0900

    refactor: Node 인터페이스 리팩토링

--- src/ui/types/index.ts
+++ src/ui/types/index.ts
@@ -1,14 +1,31 @@
+export type NodeType =
+  | 'RECTANGLE'
+  | 'POLYGON'
+  | 'TEXT'
+  | 'FRAME'
+  | 'GROUP'
+  | 'VECTOR'
+  | 'INSTANCE'
+  | 'BOOLEAN_OPERATION';
+
 export interface Node {
   id: number;
   name: string;
+  type: NodeType;
   x: number;
   y: number;
   width: number;
   height: number;
-  type: string;
-  children?: Array<Node>;
-  geometry?: string;
+  visible: boolean;
   rotation?: number;
   cornerRadius?: number;
   depth?: number;
+  fills?: Paint[];
+  strokes?: Paint[];
+  geometry?: string;
+  children?: Node[];
+  characters?: string;
+  fontSize?: number;
+  textAlignHorizontal?: 'LEFT' | 'CENTER' | 'RIGHT';
+  textAlignVertical?: 'TOP' | 'CENTER' | 'BOTTOM';
 }
