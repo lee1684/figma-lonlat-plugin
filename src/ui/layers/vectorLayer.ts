4841ce9b8dc3bba2e7423234c05442ab00d32de6
leejeongjun <backbone94@ajou.ac.kr>
2024-07-26 10:55:20 +0900

    refactor: createVectorLayer() 함수 리팩토링
    - 레이어의 스타일(frameStyle)을 createVectorLayer()로부터 분리
    - 불필요한 중심 circle 스타일 제거

--- src/ui/layers/vectorLayer.ts
+++ src/ui/layers/vectorLayer.ts
@@ -1,50 +1,27 @@
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
-import { Point } from 'ol/geom';
-import { getCenter } from 'ol/extent';
+import { Fill, Stroke, Style, Icon } from 'ol/style';
 import { FeatureLike } from 'ol/Feature';
+
+const frameStyle = new Style({
+  geometry: (feature) => {
+    const modifyGeometry = feature.get('modifyGeometry');
+    return modifyGeometry ? modifyGeometry.geometry : feature.getGeometry();
+  },
+  fill: new Fill({
+    color: 'rgba(255, 0, 0, 0.2)',
+  }),
+  stroke: new Stroke({
+    color: '#ff0000',
+    width: 2,
+  }),
+});
 
 export const createVectorLayer = (
   source: VectorSource<FeatureLike>,
 ): VectorLayer<FeatureLike> => {
-  const vectorLayerStyle = new Style({
-    geometry: (feature) => {
-      const modifyGeometry = feature.get('modifyGeometry');
-      return modifyGeometry ? modifyGeometry.geometry : feature.getGeometry();
-    },
-    fill: new Fill({
-      color: 'rgba(255, 0, 0, 0.2)',
-    }),
-    stroke: new Stroke({
-      color: '#ff0000',
-      width: 2,
-    }),
-  });
-
   return new VectorLayer({
     source,
-    style: (feature) => {
-      const styles = [vectorLayerStyle];
-      const modifyGeometry = feature.get('modifyGeometry');
-      const geometry = modifyGeometry
-        ? modifyGeometry.geometry
-        : feature.getGeometry();
-      const center = getCenter(geometry.getExtent());
-      if (center) {
-        styles.push(
-          new Style({
-            geometry: new Point(center),
-            image: new CircleStyle({
-              radius: 4,
-              fill: new Fill({
-                color: '#ff3333',
-              }),
-            }),
-          }),
-        );
-      }
-      return styles;
-    },
+    style: [frameStyle],
   });
 };
